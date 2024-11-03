7311dc338dc3e02ea7d601117e858317f2842a8f
cassigui <caguilherme14@gmail.com>
2024-11-03 15:38:12 -0300

    Consumindo a api

--- api/index.php
+++ api/index.php
@@ -1,5 +1,14 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
 use Illuminate\Container\Container;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Routing\Router;
--- frontend/src/components/formularioLogin.tsx
+++ frontend/src/components/formularioLogin.tsx
@@ -1,22 +1,44 @@
 import "./formularioLogin.css"
 import { Link } from 'react-router-dom';
+import axios from "axios"
+import { useState } from "react";
 
 function FormularioLogin() {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async (event: any) => {
+    event.preventDefault();
+
+    console.log(username)
+
+    try {
+      const response = await axios.post('http://localhost:8000/api/login', {
+        username: username,
+        password: password,
+      });
+      console.log('Resposta:', response.data)
+    } catch (error) {
+      console.error('Erro ao fazer login:', error)
+    }
+  }
+
   return (
     <>
       <div className="containerLogin">
-        <form action="submit">
+        <form onSubmit={handleLogin}>
           <h1>Login</h1>
 
           <label htmlFor="username">Username</label>
-          <input type="text" placeholder="Username:"/>
+          <input type="text" placeholder="Username:" onChange={(e) => { setUsername(e.target.value) }} />
 
           <label htmlFor="">Senha</label>
-          <input type="password" placeholder="Senha:"/>
+          <input type="password" placeholder="Senha:" onChange={(e) => { setPassword(e.target.value) }} />
 
 
-          <input type="submit" className="actionButtonPrimary" value={"Acessar"}/>
-          <Link to ="/cadastro" className="linkCadastro"><input type="button" className="actionButtonSecondary" value={"Cadastrar"}/></Link>
+          <input type="submit" className="actionButtonPrimary" value={"Acessar"} />
+          <Link to="/cadastro" className="linkCadastro"><input type="button" className="actionButtonSecondary" value={"Cadastrar"} /></Link>
         </form>
       </div>
     </>
