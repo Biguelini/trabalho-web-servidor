b8d318334e10fac7d12c001d791b122e34fdd2a8
RaayaneCustodio <108167166+RaayaneCustodio@users.noreply.github.com>
2024-11-02 21:08:19 -0300

    Alterando html e css do carrinho

--- frontend/src/pages/carrinho.tsx
+++ frontend/src/pages/carrinho.tsx
@@ -1,8 +1,20 @@
+import './carrinho.css';
 
 function Carrinho() {
   return (
     <>
-      <h1>Carrinho</h1>
+      <div className='carrinho-container'>
+        <div className="carrinho-titulo">
+          <h1>Carrinho</h1>
+        </div>
+        <div>
+          <ul className="selecionar">
+            <li className='passo'>Carrinho</li>
+            <li className='passo'>Identificação</li>
+            <li className='passo'>Pagamento</li>
+          </ul>
+        </div>
+      </div>
     </>
   )
 }
